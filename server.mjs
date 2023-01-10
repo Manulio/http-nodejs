835f947cdcf08109957c499637a3bb035f5489d6
Manuel Pallares <50201287+Manulio@users.noreply.github.com>
2023-01-10 16:52:09 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,36 @@
-import { createServer } from "http";
+const express = require("express");
+const app = express();
 
-createServer((req, res) => {
-	res.write("Hello World!");
-	res.end();
-}).listen(process.env.PORT);
+app.use(express.json());
+
+const students = [
+	{ id: 1, name: "pepe" },
+	{ id: 2, name: "jorge" },
+];
+
+app.get("/", (req, res) => {
+	res.send("CVS api");
+});
+
+app.get("/api/students", (req, res) => {
+	res.send(students);
+});
+
+app.get("/api/students/:id", (req, res) => {
+	const student = students.find((c) => c.id === parseInt(req.params.id));
+	if (student) {
+		res.send(students);
+	}
+});
+
+app.post("/api/students/", (req, res) => {
+	const student = {
+		id: students.length + 1,
+		name: req.body.name,
+	};
+	students.push(student);
+	res.send(student);
+});
+
+const port = process.env.PORT || 80;
+app.listen(port, () => console.log(`listening on port ${port}`));
